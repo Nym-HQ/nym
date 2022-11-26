e3b597bd8c5da6f4e161bbb9a23360bd6c3e36c4
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2022-11-26 16:29:14 -0500

    adding new tag

--- src/graphql/resolvers/queries/tags/index.ts
+++ src/graphql/resolvers/queries/tags/index.ts
@@ -5,19 +5,22 @@ export async function getTags(_, __, ctx: Context) {
 
   if (!site) return null
 
+  const tags = [
+    { name: 'website' },
+    { name: 'reading' },
+    { name: 'portfolio' },
+  ]
+
   try {
-    return [
-      { name: 'website' },
-      { name: 'reading' },
-      { name: 'portfolio' },
-      { name: 'indie' },
-      { name: 'open source' },
-    ]
-    // return await prisma.tag.findMany({
-    //   where: { siteId: site.id },
-    //   orderBy: { name: 'desc' },
-    // })
+    const dbTags = await prisma.tag.findMany({
+      where: { siteId: site.id },
+      orderBy: { name: 'asc' },
+    })
+    dbTags.forEach(tag => {
+      const index = tags.findIndex(t => t.name === tag.name)
+      if (index === -1) tags.push(tag)
+    })
   } catch (e) {
-    return []
   }
+  return tags
 }
