38fe075c72bf9be83b3aa6dc14f21a3aa3cd5395
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2022-12-06 12:20:53 -0500

    mobile screen - text overflow fix

--- src/components/ListDetail/Detail.tsx
+++ src/components/ListDetail/Detail.tsx
@@ -10,7 +10,7 @@ import { TitleBar } from './TitleBar'
 function ContentContainer(props) {
   return (
     <div
-      className="mx-auto w-full max-w-3xl px-4 py-6 pb-10 md:px-8"
+      className="mx-auto w-full max-w-3xl-screen px-4 py-6 pb-10 md:px-8"
       {...props}
     />
   )
