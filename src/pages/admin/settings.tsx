fe610a68d42849c1511656400e486768e072e422
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2023-01-04 15:41:51 -0500

    Settings page: rename "Social Information" section to "Online"

--- src/pages/admin/settings.tsx
+++ src/pages/admin/settings.tsx
@@ -266,7 +266,7 @@ function AdminSettingsPage(props) {
 
           <SubsectionSplitter />
 
-          <Subsection title="Social information">
+          <Subsection title="Online">
             <div className="grid grid-cols-6 gap-6">
               <div className="col-span-1 sm:col-span-1 text-center">
                 <div className="inline-block">
