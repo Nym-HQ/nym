f421413c8ef12d6fbf7c202be0b012c676f11d6a
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2023-02-01 12:26:03 -0500

    site selector page  - show visited sites

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { SiteRole } from '@prisma/client'
 import { NextPageContext } from 'next'
 import { useRouter } from 'next/router'
 import * as React from 'react'
@@ -19,32 +20,96 @@ import { getCommonPageProps } from '~/lib/commonProps'
 import { getSiteDomain } from '~/lib/multitenancy/client'
 
 function UserSitesList({ sites }) {
-  return sites && sites.length > 0 ? (
-    <div className="flex justify-center">
-      <ul className="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
-        {sites.map((site, i) => (
-          <li
-            key={site.site.subdomain}
-            className={
-              'text-base font-semibold px-6 py-2 border-b border-gray-200 w-full hover:bg-blue-600 hover:text-white' +
-              (i === 0 ? ' rounded-t-lg' : '') +
-              (i + 1 === sites.length ? ' rounded-b-lg' : '')
-            }
+  const router = useRouter()
+
+  const memberSites = sites?.filter(
+    (site) =>
+      site.siteRole === SiteRole.ADMIN || site.siteRole === SiteRole.OWNER
+  )
+  const visitorSites = sites?.filter((site) => site.siteRole === SiteRole.USER)
+
+  if (!sites || sites.length == 0) {
+    return (
+      <div className="flex justify-center">
+        <p className="dark:text-gray-200 text-gray-900">
+          Create your first site
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center space-y-12">
+      <div className="w-96 mb-4">
+        <h3 className="text-lg font-semibold text-gray-300 mb-2 text-center">
+          {' '}
+          Sites you manage{' '}
+        </h3>
+        {memberSites.length > 0 ? (
+          <ul className="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
+            {memberSites.map((site, i) => (
+              <li
+                key={site.site.subdomain}
+                className={
+                  'text-base font-semibold px-6 py-2 border-b border-gray-200 w-full hover:bg-blue-600 hover:text-white' +
+                  (i === 0 ? ' rounded-t-lg' : '') +
+                  (i + 1 === memberSites.length ? ' rounded-b-lg' : '')
+                }
+              >
+                <a href={`//${getSiteDomain(site.site)}`}>
+                  <h4>{getSiteDomain(site.site, false)}</h4>
+                  {site.site.parkedDomain && <h4>{site.site.parkedDomain}</h4>}
+                  <span className="text-gray-400 text-xs font-light">
+                    {site.siteRole}
+                  </span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-base dark:text-gray-200 text-gray-900">
+            You don't have your own site yet, wanna create one?
+          </p>
+        )}
+
+        <div className="flex justify-center mt-4">
+          <PrimaryButton
+            onClick={() => {
+              router.push('/create-site')
+            }}
           >
-            <a href={`//${getSiteDomain(site.site)}`}>
-              <h4>{getSiteDomain(site.site, false)}</h4>
-              {site.site.parkedDomain && <h4>{site.site.parkedDomain}</h4>}
-              <span className="text-gray-400 text-xs font-light">
-                {site.siteRole}
-              </span>
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
-  ) : (
-    <div className="flex justify-center">
-      <p className="dark:text-gray-200 text-gray-900">Create your first site</p>
+            Create a new Site
+          </PrimaryButton>
+        </div>
+      </div>
+      {visitorSites.length > 0 && (
+        <div className="w-96 mb-4">
+          <h3 className="text-lg font-semibold text-gray-300 mb-2 text-center">
+            {' '}
+            Sites you visited{' '}
+          </h3>
+          <ul className="bg-white rounded-lg border border-gray-200 w-96 text-gray-900">
+            {visitorSites.map((site, i) => (
+              <li
+                key={site.site.subdomain}
+                className={
+                  'text-base font-semibold px-6 py-2 border-b border-gray-200 w-full hover:bg-blue-600 hover:text-white' +
+                  (i === 0 ? ' rounded-t-lg' : '') +
+                  (i + 1 === memberSites.length ? ' rounded-b-lg' : '')
+                }
+              >
+                <a href={`//${getSiteDomain(site.site)}`}>
+                  <h4>{getSiteDomain(site.site, false)}</h4>
+                  {site.site.parkedDomain && <h4>{site.site.parkedDomain}</h4>}
+                  <span className="text-gray-400 text-xs font-light">
+                    {site.siteRole}
+                  </span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
@@ -53,7 +118,6 @@ function AppIntro() {
   const scrollContainerRef = React.useRef(null)
   const titleRef = React.useRef(null)
   const { data } = useContextQuery()
-  const router = useRouter()
 
   if (data?.context?.viewer) {
     const { data: userSites } = useGetSitesQuery()
@@ -71,7 +135,7 @@ function AppIntro() {
         <div className="p-4" ref={titleRef} />
 
         <Detail.ContentContainer>
-          <div className="pb-24 space-y-8 md:space-y-16">
+          <div className="pb-24 space-y-8 md:space-y-12">
             <h1 className="text-primary text-center font-sans text-xl font-semibold">
               Welcome back!
             </h1>
@@ -81,15 +145,6 @@ function AppIntro() {
           </div>
           <section>
             <UserSitesList sites={userSites?.userSites} />
-            <div className="flex justify-center mt-4">
-              <PrimaryButton
-                onClick={() => {
-                  router.push('/create-site')
-                }}
-              >
-                Add new Site
-              </PrimaryButton>
-            </div>
           </section>
         </Detail.ContentContainer>
       </Detail.Container>
