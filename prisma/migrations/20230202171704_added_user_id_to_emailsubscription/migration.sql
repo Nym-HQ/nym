c290908dbd0f09df65f15c6f48a944a12b9051c4
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2023-02-02 14:45:02 -0500

    add userId to the email subscription

---
+++ prisma/migrations/20230202171704_added_user_id_to_emailsubscription/migration.sql
@@ -0,0 +1,18 @@
+-- AlterTable
+ALTER TABLE `EmailSubscription` ADD COLUMN `userId` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `Page` MODIFY `text` TEXT NOT NULL DEFAULT '',
+    MODIFY `data` TEXT NOT NULL DEFAULT '';
+
+-- AlterTable
+ALTER TABLE `PageEdit` MODIFY `text` TEXT NOT NULL DEFAULT '',
+    MODIFY `data` TEXT NOT NULL DEFAULT '';
+
+-- AlterTable
+ALTER TABLE `Post` MODIFY `text` TEXT NOT NULL DEFAULT '',
+    MODIFY `data` TEXT NOT NULL DEFAULT '';
+
+-- AlterTable
+ALTER TABLE `PostEdit` MODIFY `text` TEXT NOT NULL DEFAULT '',
+    MODIFY `data` TEXT NOT NULL DEFAULT '';
