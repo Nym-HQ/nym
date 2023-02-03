c5d00f1d35a52a0014a4e34da73999fe97694437
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2023-02-03 15:58:57 -0500

    separate frontend and backend code

--- src/lib/newsletter/index.tsx
+++ src/lib/newsletter/index.tsx
@@ -51,27 +51,3 @@ export async function getNewsletterProvider(
 
   return provider
 }
-
-export const newsletterProviders = ['Mailchimp']
-
-export const newsletterProviderDetails = {
-  Mailchimp: {
-    name: 'Mailchimp',
-    setting1: 'API Key (Required)',
-    setting2: 'Audience List ID (Optional)',
-    setting3: false,
-    help_text: (
-      <span>
-        Mailchimp is a marketing automation platform and email marketing
-        service.
-        <br />
-        You can create a free account <a href="https://mailchimp.com/">here</a>.
-        <br />
-        Also read <a href="https://mailchimp.com/help/about-api-keys/">
-          this
-        </a>{' '}
-        to see how to get your API keys
-      </span>
-    ),
-  },
-}
