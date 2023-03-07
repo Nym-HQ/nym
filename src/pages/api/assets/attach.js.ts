ca6505ecd7671da3654c0157ec103c78752cea10
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2023-03-07 09:18:21 -0500

    linting

--- src/pages/api/assets/attach.js.ts
+++ src/pages/api/assets/attach.js.ts
@@ -6,9 +6,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const context = await getContext({ req, res })
 
   // remove <script> tags
-  const js = (context.site?.attach_js || '').replace(/^(\s*<script[^>]*>)|(<\/script>\s*)$/g, '');
-  res
-    .setHeader('Content-Type', 'application/javascript')
-    .send(js)
+  const js = (context.site?.attach_js || '').replace(
+    /^(\s*<script[^>]*>)|(<\/script>\s*)$/g,
+    ''
+  )
+  res.setHeader('Content-Type', 'application/javascript').send(js)
   res.end()
 }
