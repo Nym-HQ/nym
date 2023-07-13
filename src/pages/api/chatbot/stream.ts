a73d3db883c8bdea05e667d65c211f8f6a1d2f9f
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2023-07-13 14:38:31 -0400

    increase level of logging for chat api

--- src/pages/api/chatbot/stream.ts
+++ src/pages/api/chatbot/stream.ts
@@ -94,7 +94,7 @@ export default async function handler(req: Request) {
         context: promptContext.join('\n\n'),
         history: '',
       })
-      console.debug('promptString', promptString)
+      console.info('promptString', promptString)
 
       const chatMessage: ChatCompletionRequestMessage = {
         role: 'user',
@@ -120,7 +120,7 @@ export default async function handler(req: Request) {
         context: promptContext.join('\n\n'),
         history,
       })
-      console.debug('promptString', promptString)
+      console.info('promptString', promptString)
 
       completion = await openai.createCompletion({
         model: 'text-davinci-003', // 'gpt-3.5-turbo',
