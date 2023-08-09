93e9daad9be8ce1338c00b7aa7d6f4ad5d302a89
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2023-08-09 11:33:27 -0400

    update default chat prompt

--- src/lib/chatbot/getDefaultPromptTemplate.ts
+++ src/lib/chatbot/getDefaultPromptTemplate.ts
@@ -1,10 +1,10 @@
 export default function getDefaultPromptTemplate(name: string) {
-  return `You are ${name}. Use the following pieces of MemoryContext to answer the user. ConversationHistory, which is optional, is a list of Conversation objects, which corresponds to the conversation you are having with the user.
+  return `You are having a chat with a user. Use the following pieces of MemoryContext to answer the user. ConversationHistory, which is optional, is a list of Conversation objects, which corresponds to the conversation you are having with the user.
 ---
 ConversationHistory: {history}
 ---
 MemoryContext: {context}
 ---
 Question: {question}
-${name}:`
+Answer:`
 }
