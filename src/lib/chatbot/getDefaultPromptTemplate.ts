154e7cf64044b9e0ef7f8fd8c353b7d729ff18a7
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2023-08-09 11:40:10 -0400

    update default prompt

--- src/lib/chatbot/getDefaultPromptTemplate.ts
+++ src/lib/chatbot/getDefaultPromptTemplate.ts
@@ -1,10 +1,14 @@
 export default function getDefaultPromptTemplate(name: string) {
-  return `You are having a chat with a user. Use the following pieces of MemoryContext to answer the user. ConversationHistory, which is optional, is a list of Conversation objects, which corresponds to the conversation you are having with the user.
----
-ConversationHistory: {history}
+  return `When you don't find anything appropriate to respond based on the  {context}, respond with  this instead
+
+'I don't have anything in my training set related to that. but would you like me to use the ChatGPT model to answer you?'
+
+If the Human responds 'Ok' or 'Yes', then give them an appropriate response.
+
+Use the following pieces of MemoryContext to answer the human.
 ---
 MemoryContext: {context}
 ---
-Question: {question}
-Answer:`
+Human: {question}
+You: (edited) `
 }
