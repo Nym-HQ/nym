a81e51419fba010a74399ea7c8fc02cc84d49c21
klokt-valg <20753260+klokt-valg@users.noreply.github.com>
2023-09-01 09:25:04 -0400

    adjust number of questions to suggest

--- src/lib/chatbot/getDefaultPromptTemplate.ts
+++ src/lib/chatbot/getDefaultPromptTemplate.ts
@@ -1,13 +1,16 @@
 export default function getDefaultPromptTemplate(
   name: string,
-  includeHistory = false
+  includeHistory = false,
+  suggestQuestions = 3
 ) {
   return (
     `Answer to the Human's question at best you can, based on chat history and Memory Context.` +
     ` When you don't find anything appropriate to respond from Memory Context, reply with this instead: 'I don't have anything in my training set related to that. but would you like me to use the ChatGPT model to answer you?'.` +
     ` If the Human responds 'Ok' or 'Yes', then give them an appropriate response.` +
-    ` At the end of your answer, suggest the Human up to 4 questions to ask you based on the current Memory Context.` +
-    ` Prefix each suggested questions with '>> ' and suffix with a newline.` +
+    (suggestQuestions > 0
+      ? ` At the end of your answer, suggest the Human up to ${suggestQuestions} questions to ask you based on the current Memory Context.` +
+        ` Prefix each suggested questions with '>> ' and suffix with a newline.`
+      : '') +
     `\n>> Memory Context: {context}\n` +
     (includeHistory ? `\n>> Chat History: {history} : '')` : '') +
     `\n--\n` +
