ab32d38f8d41d96eedc886c15061f0a3491d795a
alpha8eta <117678345+alpha8eta@users.noreply.github.com>
2024-03-17 15:16:56 -0400

    rm seacret

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -262,7 +262,6 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const isAuth0LoginEnabled =
     (process.env.AUTH0_CLIENT_ID &&
       process.env.AUTH0_CLIENT_SECRET &&
-      process.env.AUTH0_SECRET &&
       process.env.AUTH0_BASE_URL &&
       process.env.AUTH0_ISSUER_BASE_URL) ||
     false
