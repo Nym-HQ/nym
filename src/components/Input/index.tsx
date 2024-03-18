6a2bdff1151efed774c62e70253844ddececf709
alpha8eta <117678345+alpha8eta@users.noreply.github.com>
2024-03-18 10:53:00 -0400

    "react-textarea-autosize": "^8.5.3",

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-// import TextareaAutosize from 'react-autosize-textarea'
+import TextareaAutosize from 'react-textarea-autosize'
 
 const styles =
   'w-full rounded-md text-primary px-4 py-2 text-primary bg-gray-1000 dark:bg-white dark:bg-opacity-5 bg-opacity-5 hover border-gray-200 dark:border-gray-700'
@@ -10,7 +10,7 @@ export function Input({ className = styles, ...props }) {
 
 export function Textarea({ maxRows = 8, rows = 1, ...props }) {
   return (
-    <Textarea
+    <TextareaAutosize
       maxRows={maxRows}
       rows={rows}
       className={`${styles} block`}
